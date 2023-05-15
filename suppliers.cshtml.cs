bd29da69d4bc19ad7d9081df746869ddb179f728
thakur17 <42466300+thakur17@users.noreply.github.com>
2023-05-15 03:50:57 -0400

    Add files via upload

---
+++ suppliers.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using NorthwindEntitiesLib;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace northwindapp
+{
+    public class SuppliersModel : PageModel
+    {
+        private Northwind db;
+        public SuppliersModel(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
+
+        [BindProperty]
+        public Supplier Supplier { get; set; }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                db.Suppliers.Add(Supplier);
+                db.SaveChanges();
+                return RedirectToPage("/suppliers");
+            }
+            return Page();
+        }
+        public IEnumerable<string> Suppliers { get; set; }
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind Web Site - Suppliers";
+           Suppliers = db.Suppliers.Select(s => s.CompanyName);
+        }
+    } 
+
+    }
